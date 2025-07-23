c7fc36bf19a2dc53346a942f07a8b5843740f79b
Ritik Saxena <134218041+Inversion-04@users.noreply.github.com>
2025-07-23 23:41:53 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -73,7 +73,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2025
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            ritiksaxena
+                            hiteshchoudhary
                         </a>
                         . All Rights Reserved.
                     </span>
